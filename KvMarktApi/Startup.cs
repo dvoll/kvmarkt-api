9dcbbb7ab44fe64d6c409ee85345df0187ab99b8
DESKTOP-GTE3T9A\Dario <dariovoll@gmail.com>
2018-02-11 21:25:08 +0100

    Debug Information in Release Version

--- KvMarktApi/Startup.cs
+++ KvMarktApi/Startup.cs
@@ -90,7 +90,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext con)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || true)
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
